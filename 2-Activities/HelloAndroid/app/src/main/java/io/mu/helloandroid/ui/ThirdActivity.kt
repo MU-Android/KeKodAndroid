0811ba98268022d3717ec25af812f71130618893
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-27 20:22:31 +0300

    feat: Created MainActivity and learned about Activities and AndroidManifest.xml

---
+++ 2-Activities/HelloAndroid/app/src/main/java/io/mu/helloandroid/ui/ThirdActivity.kt
@@ -0,0 +1,15 @@
+package io.mu.helloandroid.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import io.mu.helloandroid.R
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
