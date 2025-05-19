21c7e2e85440e15de95c8c4ed1fe9931217be58b
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-19 20:28:17 +0300

    feat: Implement navigation using Navigation Graph (NavGraph)

--- 7-AppNavigation/AppNavigation2/app/src/main/java/io/mu/appnavigation2/DashboardFragment.kt
+++ 7-AppNavigation/AppNavigation2/app/src/main/java/io/mu/appnavigation2/DashboardFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class DashboardFragment : Fragment() {
 
@@ -13,5 +15,15 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnOpenProfile = view.findViewById<Button>(R.id.btnOpenProfile)
+        btnOpenProfile.setOnClickListener {
+//            findNavController().navigate(R.id.action_dashboardFragment_to_profileFragment)
+            findNavController().navigate(R.id.profileFragment)
+        }
     }
 }
