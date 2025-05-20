50832269b7120654828b8695bbaba8f8f3f9d2e1
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-20 23:32:01 +0300

    feat: Jetpack Navigation recap and practice

---
+++ 8-AppNavigation/JetpackNavigation/app/src/main/java/io/mu/jetpacknavigation/MainActivity.kt
@@ -0,0 +1,60 @@
+package io.mu.jetpacknavigation
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        getNavControllerViaFragmentSupportFragmentManager()
+//        getNavControllerViaViewSupportFragmentManager()
+
+//        getNavControllerViaFragmentFindViewById()
+//        getNavControllerViaViewFindViewById()
+
+        getNavControllerViaFragmentFindNavController()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+//        getNavControllerViaViewFindViewById()
+        getNavControllerViaFragmentViewFindNavController()
+
+    }
+
+    fun getNavControllerViaFragmentSupportFragmentManager(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+
+    fun getNavControllerViaViewSupportFragmentManager(){
+        val navHostFragmentView = supportFragmentManager.findFragmentById(R.id.navHostFragmentView) as NavHostFragment
+        val navControllerView = navHostFragmentView.navController
+    }
+
+    fun getNavControllerViaFragmentFindViewById(){
+        val navHostFragment = findViewById<View>(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+    }
+
+    fun getNavControllerViaViewFindViewById(){
+        val navHostFragmentView = findViewById<View>(R.id.navHostFragmentView)
+        val navControllerView = navHostFragmentView.findNavController()
+    }
+
+    fun getNavControllerViaFragmentFindNavController(){
+        findNavController(R.id.navHostFragment)
+    }
+
+    fun getNavControllerViaFragmentViewFindNavController(){
+        findNavController(R.id.navHostFragmentView)
+    }
+
+}
---
+++ 8-AppNavigation/JetpackNavigation/app/src/main/java/io/mu/jetpacknavigation/SettingsDialogFragment.kt
@@ -0,0 +1,16 @@
+package io.mu.jetpacknavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class SettingsDialogFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings_dialog, container, false)
+    }
+}
