dda8d421643abc91405efd004f5d2d6f13503504
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-06-01 14:22:13 +0300

    feat: Define menu contents, complete NavGraph setup, bind XML in Activity, and fix NavDrawer issues

--- 9-AppNavigationBottomNavigationViewNavDrawer/PaparaSample/app/src/main/java/com/musauyumaz/paparasample/MainActivity.kt
+++ 9-AppNavigationBottomNavigationViewNavDrawer/PaparaSample/app/src/main/java/com/musauyumaz/paparasample/MainActivity.kt
@@ -1,20 +1,81 @@
 package com.musauyumaz.paparasample
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var toolbar: Toolbar
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initViews()
+        initEvents()
+    }
+
+    private fun initViews() {
+        toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.dashboardFragment,
+            R.id.qrDialogFragment,
+            R.id.moneyTransferDialogFragment,
+            R.id.paymentFragment,
+            R.id.paparaCardFragment,
+            R.id.notificationFragment,
+            R.id.searchAtmFragment,
+            R.id.withdrawDepositFragment
+        ), drawerLayout)
+    }
+
+    private fun initEvents(){
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+        navigationView.setupWithNavController(navHostFragment.navController)
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
+
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    finish()
+                }
+            }
+        })
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navHostFragment.navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
