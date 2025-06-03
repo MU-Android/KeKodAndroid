17f15f6ec18a1e081b0bc9f289743185c2e816f7
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-06-03 22:40:01 +0300

    feat: Studying manual dependency injection concepts and practices

---
+++ 10-DependencyInjection/DependencyInjection/app/src/main/java/com/musauyumaz/dependencyinjection/Car.kt
@@ -0,0 +1,53 @@
+package com.musauyumaz.dependencyinjection
+
+class Car(private val engine: Engine) {
+
+    fun start() {
+        engine.start()
+    }
+}
+
+class Plane(private val engine: Engine) {
+
+    fun start() {
+        engine.start()
+    }
+}
+
+
+
+interface Engine {
+    fun start()
+}
+
+class GasEngine : Engine {
+    override fun start() {
+        println("start gas engine")
+    }
+}
+
+class ElectricEngine : Engine {
+    override fun start() {
+        println("start Electric engine")
+    }
+}
+
+class HybridEngine : Engine {
+    override fun start() {
+        println("start Electric engine")
+    }
+}
+
+fun main() {
+    val gasEngine = GasEngine()
+    val car = Car(gasEngine)
+    car.start()
+
+    val electricEngine = ElectricEngine()
+    val car2 = Car(electricEngine)
+    car2.start()
+
+    val hybridEngine = HybridEngine()
+    val car3 = Car(hybridEngine)
+    car3.start()
+}
