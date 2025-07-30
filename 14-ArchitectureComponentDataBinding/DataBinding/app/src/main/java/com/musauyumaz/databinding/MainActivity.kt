adca259b319c7d8868468e4ad882701eccdbd01f
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-07-30 21:51:40 +0300

    feat: Learned usage of findViewById and its disadvantages

---
+++ 14-ArchitectureComponentDataBinding/DataBinding/app/src/main/java/com/musauyumaz/databinding/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.musauyumaz.databinding
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    lateinit var txtProfileName : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        txtProfileName = findViewById<TextView>(R.id.txtProfileName)
+
+        txtProfileName.text = "Merhaba Musa"
+        txtProfileName.setOnClickListener {
+
+        }
+    }
+}
